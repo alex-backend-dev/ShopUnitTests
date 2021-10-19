80443137a879c358a69c66a8732ce99b734c3a28
Alexander Ulyanitsky <alex.backend.dev@gmail.com>
2021-10-19 12:51:40 +0300

    Переделал тестовый класс ProductTests, откомментировал код, где была проблема, все заработало.

--- MainSolution/ShopModelUnitTests/ProductTests/ProductTests.cs
+++ MainSolution/ShopModelUnitTests/ProductTests/ProductTests.cs
@@ -6,15 +6,13 @@ namespace ShopModelUnitTests
     [TestFixture]
     public class ProductTests
     {
-        private Product? _product1;
-        private Product? _product2;
 
         [Test]
         [Description("Validate correct equality of the same objects")]
         public void Equals_CorrectEquals_ExpectedTrue()
         {
-            _product1 = new Product() { Name = "Milk", Price = 100, ProductID = 2 };
-            _product2 = new Product() { Name = "Milk", Price = 100, ProductID = 2 };
+            Product _product1 = new Product() { Name = "Milk", Price = 100, ProductID = 2 };
+            Product _product2 = new Product() { Name = "Milk", Price = 100, ProductID = 2 };
 
             Assert.IsTrue(_product1 == _product2);
         }
@@ -23,30 +21,30 @@ namespace ShopModelUnitTests
         [Description("Validate incorrect equality of different objects")]
         public void Equals_CorrectEquals_ExpectedFalse()
         {
-            _product1  = new Product() { Name = "Milk", Price = 100, ProductID = 2 };
-            _product2 = new Product() { Name = "Butter", Price = 50, ProductID = 3 };
+            Product _product3 = new Product() { Name = "Milk", Price = 100, ProductID = 2 };
+            Product _product4 = new Product() { Name = "Butter", Price = 50, ProductID = 3 };
 
-            Assert.IsFalse(_product1 == _product2);
+            Assert.IsFalse(_product3 == _product4);
         }
 
         [Test]
         [Description("Validate correct inequality of different objects")]
         public void Inequality_CorrectInequality_ExpectedTrue()
         {
-            _product1 = new Product() { Name = "Milk", Price = 100, ProductID = 2 };
-            _product2 = new Product() { Name = "Butter", Price = 50, ProductID = 3 };
+            Product _product5 = new Product() { Name = "Milk", Price = 100, ProductID = 2 };
+            Product _product6 = new Product() { Name = "Butter", Price = 50, ProductID = 3 };
 
-            Assert.IsTrue(_product1 != _product2);
+            Assert.IsTrue(_product5 != _product6);
         }
 
         [Test]
         [Description("Validate incorrect inequality of the same objects")]
         public void Inequality_CorrectInequality_ExpectedFalse()
         {
-            _product1 = new Product() { Name = "Milk", Price = 100, ProductID = 2 };
-            _product2 = new Product() { Name = "Milk", Price = 100, ProductID = 2 };
+            Product _product7 = new Product() { Name = "Milk", Price = 100, ProductID = 2 };
+            Product _product8 = new Product() { Name = "Milk", Price = 100, ProductID = 2 };
 
-            Assert.IsFalse(_product1 != _product2);
+            Assert.IsFalse(_product7 != _product8);
         }
     }
 }
